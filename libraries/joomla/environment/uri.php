92cbf9e5c18747ff2d1ce932b30003a36caac5fd
Marc Català <reskit@gmail.com>
2012-04-18 09:40:22 +0200

    Arreglat el problema amb el balancejador de càrrega amb conexions SSL

--- libraries/joomla/environment/uri.php
+++ libraries/joomla/environment/uri.php
@@ -153,7 +153,8 @@ class JURI extends JObject
 			if ($uri == 'SERVER')
 			{
 				// Determine if the request was over SSL (HTTPS).
-				if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) != 'off'))
+				if ((isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) != 'off')) ||
+				   (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
 				{
 					$https = 's://';
 				}
