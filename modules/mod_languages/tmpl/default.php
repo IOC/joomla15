d6922bd1c5d2a479fc3012cdd07b60ac6fd3f369
Marc Català <reskit@gmail.com>
2014-08-01 13:40:55 +0200

    Selector idioma: L'idoma seleccionat no és un enllaç. Mostrar la bandera i el nom de l'idioma.

--- modules/mod_languages/tmpl/default.php
+++ modules/mod_languages/tmpl/default.php
@@ -29,13 +29,18 @@ JHtml::_('stylesheet', 'mod_languages/template.css', array(), true);
 	<?php foreach($list as $language):?>
 		<?php if ($params->get('show_active', 0) || !$language->active):?>
 			<li class="<?php echo $language->active ? 'lang-active' : '';?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>">
-			<a href="<?php echo $language->link;?>">
+			<?php if (!$language->active) { ?>
+				<a href="<?php echo $language->link;?>">
+			<?php } ?>
 			<?php if ($params->get('image', 1)):?>
 				<?php echo JHtml::_('image', 'mod_languages/'.$language->image.'.gif', $language->title_native, array('title'=>$language->title_native), true);?>
+				<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
 			<?php else : ?>
 				<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
 			<?php endif; ?>
-			</a>
+			<?php if (!$language->active) { ?>
+				</a>
+			<?php } ?>
 			</li>
 		<?php endif;?>
 	<?php endforeach;?>
